d2a97ec687e9d6fe23d33424b09d891e8fd49f8d
datroiano <deantroiano123@gmail.com>
2024-02-29 14:50:16 -0500

    starting machine learn applications

--- OptionClasses/Strategies/long_straddle.py
+++ OptionClasses/Strategies/long_straddle.py
@@ -133,19 +133,19 @@ class StraddleStrategy:
         return pd.DataFrame(meta_data.items(), columns=['Metric', 'Value'])
 
 
-test = StraddleStrategy(ticker='nvda',
-                        strike=790,
-                        expiration_date='2024-03-01',
-                        quantity=1,
-                        entry_date='2024-02-28',
-                        exit_date='2024-02-28',
-                        strategy_type='long',
-                        entry_exit_period=('10:30:00', '11:30:00', '12:30:00', '16:00:00'),
-                        timespan='minute',
-                        fill_gaps=True,
-                        per_contract_commission=0.01,
-                        multiplier=1,
-                        polygon_api_key='r1Jqp6JzYYhbt9ak10x9zOpoj1bf58Zz'
-                        )
-
-print(StraddleStrategy.get_meta_data(test.run_simulation()))
+# test = StraddleStrategy(ticker='nvda',
+#                         strike=790,
+#                         expiration_date='2024-03-01',
+#                         quantity=1,
+#                         entry_date='2024-02-28',
+#                         exit_date='2024-02-28',
+#                         strategy_type='long',
+#                         entry_exit_period=('10:30:00', '11:30:00', '12:30:00', '16:00:00'),
+#                         timespan='minute',
+#                         fill_gaps=True,
+#                         per_contract_commission=0.01,
+#                         multiplier=1,
+#                         polygon_api_key='r1Jqp6JzYYhbt9ak10x9zOpoj1bf58Zz'
+#                         )
+#
+# print(test.run_simulation().columns)
